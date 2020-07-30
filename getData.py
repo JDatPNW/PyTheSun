1fb91ab76e05bd51ef8bef7782230bbec8bf44b7
David <david.richter@freenet.de>
2020-07-30 13:13:30 +0200

    resized the image and went back to while loop

--- getData.py
+++ getData.py
@@ -9,21 +9,17 @@ bounding_box = {'top': 28, 'left': 0, 'width': 800, 'height': 600}
 last_time = time.time()
 sct = mss()
 
-for i in range(1000):
-    keys = [0, 0, 0, 0]
-    if(keyboard.is_pressed('w')):
-        keys[0] = 1
+while(True):
+    keys = [0, 0]
     if(keyboard.is_pressed('a')):
-        keys[1] = 1
-    if(keyboard.is_pressed('s')):
-        keys[2] = 1
+        keys[0] = 1
     if(keyboard.is_pressed('d')):
-        keys[3] = 1
+        keys[1] = 1
     if(keyboard.is_pressed('q')):
         break
 
     screen = np.array(sct.grab(bounding_box))
-
+    screen = cv2.resize(screen, (200, 150))
     print('Frame took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
 
